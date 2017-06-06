3d7dff34a1e1dadc1bf102b611e6fb87dfd06a23
olegsugatov <oleg.sugatov@gmail.com>
2017-06-06 12:05:06 +0300

    init course

---
+++ README.md
@@ -0,0 +1,19 @@
+# webpack-react-starter
+
+```npm
+npm run dev
+```
+
++watch -minified 
+
+```npm
+npm run prod
+```
++watch -minified 
+
+
+Tasks
+---------
+
+- [ ] add prod build
+- [ ] add rComponents# react101
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+    	path: path.resolve(__dirname, '/dist')﻿,
+        filename: 'app.bundle.js'
+    },
+    module: {
+    	rules: [
+    		{
+                test: /\.scss$/, 
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                use: 'babel-loader'
+            }
+    	]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [  new HtmlWebpackPlugin({
+    	title: 'Project Demo',
+    	minify: {
+    		collapseWhitespace: true
+    	},
+    	hash: true,
+    	template: './src/index.ejs'
+ 	 })
+   ]
+}
